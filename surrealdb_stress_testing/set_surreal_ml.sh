4e067ab1f23ef91abc25b17a11aa850374ba5839
vladimirrotariu <rotariuvladimir@gmail.com>
2024-10-15 00:42:23 +0200

    create unified script to install Rust and start SurrealDB in-memory

--- surrealdb_stress_testing/set_surreal_ml.sh
+++ surrealdb_stress_testing/set_surreal_ml.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-USER="vld28"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <username> <root_password>"
+    exit 1
+fi
+
+USER="$1"
+ROOT_PASSWORD="$2"
+
+export RUST_BACKTRACE=full
 
 curl -sSf https://install.surrealdb.com | sh
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source /home/"$USER"/.cargo/env
 
-
+echo "$ROOT_PASSWORD" | sudo -S surreal start
