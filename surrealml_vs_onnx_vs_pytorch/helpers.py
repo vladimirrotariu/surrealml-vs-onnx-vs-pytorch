38316197d2f4403f89758071e5f150a31d89be4c
vladimirrotariu <rotariuvladimir@gmail.com>
2024-10-17 02:09:47 +0200

    add PyTorch and query SurrealDB case implementation

--- surrealml_vs_onnx_vs_pytorch/helpers.py
+++ surrealml_vs_onnx_vs_pytorch/helpers.py
@@ -1,10 +1,13 @@
 import subprocess
 
-def remove_existing_container_if_exists(container_name: str) -> str | None:
+
+def remove_container_if_exists(container_name: str) -> str | None:
     try:
         subprocess.run(["docker", "stop", container_name], check=True)
         subprocess.run(["docker", "rm", container_name], check=True)
         print(f"Removed existing container '{container_name}'.")
         return container_name
     except subprocess.CalledProcessError:
-        print(f"No existing container named '{container_name}' found or could not be removed.")
+        print(
+            f"No existing container named '{container_name}' found or could not be removed."
+        )
